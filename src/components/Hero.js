33ddb053c264dc421db8b41568129eff7ed13474
d.sajedi <d.sajedi@fanap.ir>
2024-11-01 10:12:14 +0330

    [improvements] - Hero minor improvement.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
         <section className="hero">
             <div className="hero-content">
                 <h1>Decentralized Storage. AI-Powered Efficiency. Empowering the Future of Web3</h1>
-                <p>Leverage AI-driven performance, privacy-first storage, and decentralized architecture for secure, low-cost storage optimized for Ethereum and EVM-compatible ecosystems</p>
+                <p className="w-3/4 justify-self-center">Leverage AI-driven performance, privacy-first storage, and decentralized architecture for secure, low-cost storage optimized for Ethereum and EVM-compatible ecosystems</p>
                 <div className="hero-buttons">
                     <button className="primary-btn">Start Storing</button>
                     <button className="secondary-btn">Learn More</button>
