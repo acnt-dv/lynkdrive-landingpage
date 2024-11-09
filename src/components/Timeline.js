cf71a9bf485fe14935e49acf180ab21b0d670ed4
d.sajedi <d.sajedi@fanap.ir>
2024-11-09 10:27:51 +0330

    [improvements] - data improvement in Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,10 +3,17 @@ import './Timeline.css';
 
 const Timeline = () => {
     const events = [
-        { id: 1, title: 'Q1 - 2018', description: 'Lorem ipsum dolor sit amet' },
-        { id: 2, title: 'Q2 - 2018', description: 'Lorem ipsum dolor sit amet' },
-        { id: 3, title: 'Q3 - 2018', description: 'Lorem ipsum dolor sit amet' },
-        { id: 4, title: 'Q4 - 2018', description: 'Lorem ipsum dolor sit amet' },
+        { id: 1, title: 'Q4 2024 - Q1 2025', description: 'Launch of LynkDrive main net\n' +
+                '                    Launch marketing campaigns to attract storage node operators\n' +
+                '                    Early rewards distribution to storage contributors using LYND tokens' },
+        { id: 2, title: 'Q2 - Q4 2025', description: 'Global expansion of the storage node network\n' +
+                '                    Integration of AI-powered Proof of Space Quality for enhanced node validation\n' +
+                '                    Launch mobile apps for seamless user experience' },
+        { id: 3, title: '2026', description: 'Introduction of Object Storage Services\n' +
+                '                    Listing LYND tokens on major exchanges\n' +
+                '                    Develop partnerships with leading Web3 projects' },
+        { id: 4, title: 'Beyond 2026', description: 'Expansion into decentralized computing and bandwidth services\n' +
+                '                    Establish LynkDrive as a global leader in decentralized storage' },
     ];
 
     return (
